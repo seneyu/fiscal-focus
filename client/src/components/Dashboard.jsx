b448e86e06be74ec538ea62ade08847a4ea0e30e
Stephany Ho <shohk39@gmail.com>
2025-05-19 11:23:43 -0700

    display current month, modify spending page with donut chart

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -6,6 +6,7 @@ import ExpenseDetailsForm from './ExpenseDetailsForm';
 import ExpenseDetailsChart from './ExpenseDetailsChart';
 import ExpenseDetailsTable from './ExpenseDetailsTable';
 import CustomSummary from './CustomSummary';
+import CashFlow from './CashFlow';
 
 const CustomTabPanel = (props) => {
   const { children, value, index, ...other } = props;
@@ -36,10 +37,30 @@ const Dashboard = () => {
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const [value, setValue] = useState(0);
+  const [date, setDate] = useState('');
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  // fetch current month string
+  useEffect(() => {
+    const getCurrMonth = () => {
+      const now = new Date();
+      const firstDay = new Date(now.getFullYear(), now.getMonth(), 1);
+      const lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+
+      // format both dates
+      const options = { month: 'long', day: 'numeric', year: 'numeric' };
+      const formattedFirstDay = firstDay.toLocaleDateString('en-US', options);
+      const formattedLastDay = lastDay.toLocaleDateString('en-US', options);
+
+      const periodString = `${formattedFirstDay} - ${formattedLastDay}`;
+      setDate(periodString);
+    };
+
+    getCurrMonth();
+  }, [navigate]);
 
   useEffect(() => {
     const fetchExpenses = async () => {
@@ -64,13 +85,22 @@ const Dashboard = () => {
 
         setUser(user);
 
+        // get current month range
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = now.getMonth();
+        const firstDay = new Date(year, month, 1).toISOString().slice(0, 10);
+        const lastDay = new Date(year, month + 1, 0).toISOString().slice(0, 10);
+
         const { data, error } = await supabase
           .from('expenses')
           .select('*')
           .eq('user_id', user.id)
+          .gte('date', firstDay)
+          .lte('date', lastDay)
           .order('date', { ascending: false });
 
-        // console.log('Expenses data:', data);
+        console.log('Expenses data:', data);
 
         if (error) {
           setError(error.message);
@@ -80,6 +110,7 @@ const Dashboard = () => {
 
         setExpenses(data);
       } catch (err) {
+        setError(error.message);
         console.error('Error fetching expenses:', err);
       } finally {
         setLoading(false);
@@ -90,7 +121,9 @@ const Dashboard = () => {
   }, [navigate]);
 
   const addNewExpense = (newExpense) => {
-    setExpenses((prevExpenses) => [newExpense, ...prevExpenses]);
+    if (isCurrentMonth(newExpense.date)) {
+      setExpenses((prevExpenses) => [newExpense, ...prevExpenses]);
+    }
   };
 
   const onExpenseDeleted = (deletedId) => {
@@ -107,6 +140,16 @@ const Dashboard = () => {
     );
   };
 
+  // check if modified entry is for current month
+  const isCurrentMonth = (dateString) => {
+    const now = new Date();
+    const expenseDate = new Date(dateString);
+    return (
+      expenseDate.getFullYear() === now.getFullYear() &&
+      expenseDate.getMonth() === now.getMonth()
+    );
+  };
+
   return (
     <Container
       sx={{ display: 'flex', justifyContent: 'center' }}
@@ -118,16 +161,24 @@ const Dashboard = () => {
             onChange={handleChange}
             textColor="primary"
             aria-label="dashboard tabs">
-            <Tab label="Expense Overview" {...a11yProps(0)} />
-            <Tab label="Expense Details" {...a11yProps(1)} />
+            <Tab label="Cash Flow" {...a11yProps(0)} />
+            <Tab label="Spending" {...a11yProps(1)} />
             <Tab label="Custom Summary" {...a11yProps(2)} />
           </Tabs>
         </Box>
+        <CustomTabPanel value={value} index={0}>
+          <CashFlow date={date} />
+        </CustomTabPanel>
         <CustomTabPanel value={value} index={1}>
           <Container>
-            <Box sx={{ display: 'inline-flex', alignitems: 'center' }}>
-              <ExpenseDetailsForm onExpenseAdded={addNewExpense} />
-              <ExpenseDetailsChart expenses={expenses} loading={loading} />
+            {/* <Box sx={{ display: 'inline-flex', alignitems: 'center' }}> */}
+            <Box>
+              {/* <ExpenseDetailsForm onExpenseAdded={addNewExpense} /> */}
+              <ExpenseDetailsChart
+                date={date}
+                expenses={expenses}
+                loading={loading}
+              />
             </Box>
             <ExpenseDetailsTable
               expenses={expenses}
--- client/src/components/ExpenseDetailsChart.jsx
+++ client/src/components/ExpenseDetailsChart.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useRef } from 'react';
-
-import { Typography, Box } from '@mui/material';
+import { Typography, Box, Card } from '@mui/material';
 import * as d3 from 'd3';
 
-const ExpenseDetailsChart = ({ expenses, loading }) => {
+const ExpenseDetailsChart = ({ date, expenses, loading }) => {
   const d3Container = useRef(null);
 
   // load d3 visualization expenses object is retrieved
@@ -31,18 +30,18 @@ const ExpenseDetailsChart = ({ expenses, loading }) => {
         d.percentage = (d.data.amount / total) * 100;
       });
 
-      const width = 650;
-      const height = 550;
-      const margin = 120;
-      const radius = Math.min(width, height) / 2 - margin;
+      const width = 700;
+      const height = 400;
+      const margin = 40;
+      const radius = Math.min(height - margin * 2, width / 2 - margin * 3) / 2;
 
       const svg = d3
         .select(d3Container.current)
         .append('svg')
-        .attr('width', width)
-        .attr('height', height)
-        .append('g')
-        .attr('transform', `translate(${width / 2}, ${height / 2})`);
+        .attr('viewBox', `0 0 ${width} ${height}`)
+        .attr('width', '100%')
+        .attr('height', '100%')
+        .attr('preserveAspectRatio', 'xMidYMid meet');
 
       const color = d3
         .scaleOrdinal()
@@ -63,19 +62,26 @@ const ExpenseDetailsChart = ({ expenses, loading }) => {
         ])
         .range(d3.schemeCategory10);
 
-      const arc = d3.arc().innerRadius(0).outerRadius(radius);
-      const arcs = svg
+      const donutGroup = svg
+        .append('g')
+        .attr('transform', `translate(${width / 4}, ${height / 2})`);
+
+      const legendGroup = svg
+        .append('g')
+        .attr('transform', `translate(${width * 0.55}, ${height / 2 - 120})`);
+
+      const arc = d3
+        .arc()
+        .innerRadius(radius * 0.75)
+        .outerRadius(radius);
+
+      // draw arcs in donutGroup
+      const arcs = donutGroup
         .selectAll('.arc')
         .data(pieData)
         .enter()
         .append('g')
         .attr('class', 'arc');
-
-      // outer arc for positioning labels
-      const outerArc = d3
-        .arc()
-        .innerRadius(radius * 1.1)
-        .outerRadius(radius * 1.1);
 
       // add paths (pie slices)
       const paths = arcs
@@ -85,74 +91,42 @@ const ExpenseDetailsChart = ({ expenses, loading }) => {
         })
         .attr('d', arc);
 
-      // add amount items
-      arcs
+      // add spending total and text in center of donut chart
+      donutGroup
         .append('text')
-        .attr('transform', (d) => {
-          // only position text if the slice is big enough
-          const [x, y] = arc.centroid(d);
-          return `translate(${x * 0.7}, ${y * 0.7})`;
-        })
-        .attr('dy', '.25em')
         .attr('text-anchor', 'middle')
-        .style('font-size', '12px')
-        .style('fill', 'white')
-        // .style('font-weight', 'bold')
-        // only show amount if the slice is big enough
-        .text((d) => {
-          return d.percentage > 5 ? `$${d.data.amount.toFixed(2)}` : '';
-        });
+        .style('font-size', '1.2rem')
+        .style('fill', '#333')
+        .text(
+          `$${total.toLocaleString(undefined, {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+          })}`
+        );
 
-      // separate larger and smaller slices foro different label treatments
-      const largeSlices = arcs.filter((d) => d.percentage >= 3);
-      const smallSlices = arcs.filter((d) => d.percentage < 3);
-
-      // add lines and labels for large slices
-      largeSlices
-        .append('polyline')
-        .attr('points', (d) => {
-          const midAngle = d.startAngle + (d.endAngle - d.startAngle) / 2;
-          const x = Math.sin(midAngle) * radius;
-          const y = -Math.cos(midAngle) * radius;
-
-          const pos = outerArc.centroid(d);
-          const x2 = pos[0] * 1.12;
-          const y2 = pos[1] * 1.12;
-
-          return [[x, y], pos, [x2, y2]];
-        })
-        .style('fill', 'none')
-        .style('stroke', 'black')
-        .style('stroke-width', '1px');
-
-      // category labels
-      largeSlices
+      donutGroup
         .append('text')
-        .attr('transform', (d) => {
-          const pos = outerArc.centroid(d);
-          const x = pos[0] * 1.2;
-          const y = pos[1] * 1.2;
-          return `translate(${x}, ${y})`;
-        })
-        .attr('text-anchor', (d) => {
-          const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2;
-          return midangle < Math.PI ? 'start' : 'end';
-        })
-        .style('font-size', '12px')
-        .style('font-weight', 'bold')
-        .text((d) => d.data.category);
+        .attr('text-anchor', 'middle')
+        .attr('dy', '1.4em')
+        .style('font-size', '1rem')
+        .style('fill', 'black')
+        .text('Total');
 
       // add legend
-      const legendRectSize = 15;
+      const legendRectSize = 10;
+      const legendSpacing = 24;
 
-      const legend = svg
+      const legendItemsPerColumn = 8;
+      const legend = legendGroup
         .selectAll('.legend')
         .data(pieData)
         .enter()
         .append('g')
         .attr('class', 'legend')
         .attr('transform', (d, i) => {
-          return `translate(${width / 2 - 150}, ${-height / 2 + 20 + i * 20})`;
+          const column = Math.floor(i / legendItemsPerColumn);
+          const row = i % legendItemsPerColumn;
+          return `translate(${column * 150}, ${row * legendSpacing})`;
         });
 
       legend
@@ -172,7 +146,7 @@ const ExpenseDetailsChart = ({ expenses, loading }) => {
             d.data.category.slice(1, d.data.category.length);
           return `${category} (${d.percentage.toFixed(1)}%)`;
         })
-        .style('font-size', '12px');
+        .style('font-size', '0.8rem');
 
       // define tooltip
       const tooltip = d3
@@ -191,17 +165,6 @@ const ExpenseDetailsChart = ({ expenses, loading }) => {
       // attach mouse event handlers to pie slices
       paths
         .on('mouseover', function (event, d) {
-          d3.select(this)
-            .transition()
-            .duration(200)
-            .attr(
-              'd',
-              d3
-                .arc()
-                .innerRadius(0)
-                .outerRadius(radius * 1.08)
-            );
-
           tooltip.style(
             'visibility',
             'visible'
@@ -231,34 +194,22 @@ const ExpenseDetailsChart = ({ expenses, loading }) => {
   }
 
   return (
-    <Box
-      mt={4}
-      sx={{
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}>
-      <Typography variant="h5" mb={2}>
-        Your Expenses Overview
-      </Typography>
-      {loading ? (
-        <Typography>Loading expenses data...</Typography>
-      ) : expenses.length === 0 ? (
-        <Typography>
-          No expense data available. Add some expenses to see your chart.
+    <Box>
+      <Card sx={{ padding: '2rem' }}>
+        <Typography sx={{ typography: 'subtitle2' }}>
+          SPENDING BY CATEGORY
         </Typography>
-      ) : (
-        <div
-          ref={d3Container}
-          style={{
-            width: '100%',
-            height: '600px',
-            display: 'flex',
-            justifyContent: 'center',
-          }}
-        />
-      )}
+        <Typography sx={{ typography: 'h6' }}>{date}</Typography>
+        {loading ? (
+          <Typography>Loading expenses data...</Typography>
+        ) : expenses.length === 0 ? (
+          <Typography>
+            No expense data available. Add some expenses to see your chart.
+          </Typography>
+        ) : (
+          <div ref={d3Container} />
+        )}
+      </Card>
     </Box>
   );
 };
